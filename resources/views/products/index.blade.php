41e37b90557ee77ce56dd00f7466957cba68f768
jenou11 <jenousdongol11@gmail.com>
2026-07-21 21:02:53 +0545

    working on searched products card

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,14 +1,17 @@
 <x-app-layout>
 <x-search />
-@if($searchedproduct)
-<x-searched-product-card :searchedproduct="$searchedproduct"/>
 
-@else
+    <div class="  flex flex-wrap ml-[125px] gap-[20px]  ">
+      @foreach ($searchedproduct as $searchedproduct)
+
+<x-searched-product :searchedproduct="$searchedproduct"/>
+@endforeach
+
     <div class="  flex flex-wrap ml-[125px] gap-[20px]  ">
       @foreach ($products as $product)
 <x-product-card :product="$product"/>
               @endforeach
             </div>
             {{-- {{ $products->links() }} --}}
-{{-- @endif --}}
+
 </x-app-layout>
