a4fd35b21bf9c12a0bfb4f22a855d4f5ca936719
jenou11 <jenousdongol11@gmail.com>
2026-07-29 12:28:20 +0545

    made the dashboard when logged in to point at admin's index page

--- routes/web.php
+++ routes/web.php
@@ -7,18 +7,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/',[ProductController::class,'index',])->name('public.products.index');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 Route::middleware(['auth','admin'])->prefix('admin')->group(function () {
-
+  Route::get('/dashboard',[AdminController::class,'index'])->name('adminproducts.adminindex');
   Route::get('/adminproducts', [AdminController::class,'index'])->name('adminproducts.adminindex');
   Route::get('/adminproducts/create', [AdminController::class,'create'])->name('adminproducts.create');
   Route::post('/adminproducts', [AdminController::class,'store'])->name('adminproducts.store');
