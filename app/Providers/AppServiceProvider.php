65826a065e6396acd7d34cf455064bc8a81c6f76
jenou11 <jenousdongol11@gmail.com>
2026-07-30 14:26:11 +0545

    did some changes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // i am unguarding the model
+        Model::unguard();
     }
 }
