f34c21a564160336e40362c6afbae37ccf53f525
jenou11 <jenousdongol11@gmail.com>
2026-08-02 12:28:06 +0545

    wanted to make the filter option work but couldn't

--- resources/views/components/productfilter.blade.php
+++ resources/views/components/productfilter.blade.php
@@ -4,9 +4,10 @@
     <form action="{{route('products.productfilter')}}" method="post" class="rounded-lg">
       @csrf
       <select name="filteroptions" id="" placeholder="select filter" class="rounded-lg">
-        select
-        <option value="0">filter</option>
-        <option name="is_featured" value="is_featured">featured products</option>
+
+        {{-- preselected option --}}
+        <option value="" selected>--filter--</option>
+        <option name="is_featured" value="is_featured" >featured products</option>
         <option  name="is_stock" value="is_stock">in stock products</option>
       </select>
       <button type="submit">submit</button>
