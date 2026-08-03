613d65ed89fd935dfc6d2fcc6a822165252a746e
jenou11 <jenousdongol11@gmail.com>
2026-08-03 23:50:10 +0545

    added product page descriptions

--- resources/views/products/about.blade.php
+++ resources/views/products/about.blade.php
@@ -1,6 +1,10 @@
 <div>
     <!-- The biggest battle is the war against ignorance. - Mustafa Kemal Atatürk -->
     <section>
-      Infusing Energy, Crafting Meaning
-    </section>
+      <h1>Infusing Energy, Crafting Meaning</h1>
+      <i>
+Handmade Crystals from the Himalayas
+At Himalayan Crystal House, we believe crystals carry energy that aligns with your soul. Every piece we create is handcrafted with care, purpose, and deep spiritual significance.</i>
+<p>At Himalayan Crystal House, we believe crystals carry energy that aligns with your soul. Every piece we create is handcrafted with care, purpose, and deep spiritual significance.</p>
+</section>
 </div>
