247a7a2046d0e1fa411a7d36d43db1e65c39734e
jenou11 <jenousdongol11@gmail.com>
2026-08-04 23:50:24 +0545

    added grid styles to the about page section

--- resources/views/products/about.blade.php
+++ resources/views/products/about.blade.php
@@ -1,12 +1,22 @@
-<div>
+<div class="bg-red-600">
     <!-- The biggest battle is the war against ignorance. - Mustafa Kemal Atatürk -->
-    <section>
-      <h1>Infusing Energy, Crafting Meaning</h1>
-      <i>
-Handmade Crystals from the Himalayas
-At Himalayan Crystal House, we believe crystals carry energy that aligns with your soul. Every piece we create is handcrafted with care, purpose, and deep spiritual significance.</i>
-<p>At Himalayan Crystal House, we believe crystals carry energy that aligns with your soul. Every piece we create is handcrafted with care, purpose, and deep spiritual significance.</p>
-<button> Explore Our Collection</button>
-<img src="{{asset('images/aboutpagehero.jpg')}}" class="max-w-2" alt="">
-</section>
+    <section class="grid grid-cols-3 gap-4 bg-red-300">
+        <div>
+
+            <h1>Infusing Energy, Crafting Meaning</h1>
+            <i>
+                Handmade Crystals from the Himalayas
+                At Himalayan Crystal House, we believe crystals carry energy that aligns with your soul. Every piece we
+                create is handcrafted with care, purpose, and deep spiritual significance.</i>
+            <p>At Himalayan Crystal House, we believe crystals carry energy that aligns with your soul. Every piece we
+                create is handcrafted with care, purpose, and deep spiritual significance.</p>
+            <button> Explore Our Collection</button>
+
+        </div>
+        <div class="w-full">
+
+            <img src="{{ asset('images/aboutpagehero.jpg') }}" alt="">
+        </div>
+
+    </section>
 </div>
