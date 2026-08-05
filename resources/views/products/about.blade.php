51716e4191fc7d8947cb089d9b8c71fc8bcb7572
jenou11 <jenousdongol11@gmail.com>
2026-08-05 10:16:50 +0545

    made some changes on the about page, working on the size of image rn

--- resources/views/products/about.blade.php
+++ resources/views/products/about.blade.php
@@ -1,22 +1,20 @@
 <x-app-layout>
 <div class="bg-red-600">
     <!-- The biggest battle is the war against ignorance. - Mustafa Kemal Atatürk -->
-    <section class="grid grid-cols-2 gap-4 bg-red-300">
-        <div>
+    <section class="grid grid-cols-2 gap-5 bg-red-300">
+        <div class="bg-yellow-300 flex flex-col items-start gap-5 ml-[65px] ">
 
-            <h1>Infusing Energy, Crafting Meaning</h1>
-            <i>
-                Handmade Crystals from the Himalayas
-                At Himalayan Crystal House, we believe crystals carry energy that aligns with your soul. Every piece we
-                create is handcrafted with care, purpose, and deep spiritual significance.</i>
+            <h1 class="text-6xl mt-[150px] font-bold ">Infusing Energy, Crafting Meaning</h1>
+            <i class="text-3xl ">
+                Handmade Crystals from the Himalayas</i>
+
             <p>At Himalayan Crystal House, we believe crystals carry energy that aligns with your soul. Every piece we
                 create is handcrafted with care, purpose, and deep spiritual significance.</p>
-            <button> Explore Our Collection</button>
+            <button class=" bg-green-600 text-white rounded-md"> Explore Our Collection</button>
 
         </div>
-        <div class="w-full">
-
-            <img src="{{ asset('images/aboutpagehero.jpg') }}" alt="" width="2200" height="1331">
+        <div class=" ">
+           <img src="{{ asset('images/aboutpagehero.jpg') }}" alt="" width="500"height="331" class="rounded-3xl" >
         </div>
 
     </section>
