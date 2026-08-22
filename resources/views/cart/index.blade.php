2a1047fb1591498bbf2a52a51fbc7a019643e7e8
jenou11 <jenousdongol11@gmail.com>
2026-08-22 17:11:48 +0545

    made some changes

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -3,8 +3,10 @@
  @foreach($cartItems as $product)
  <div class="">
 
+   <p> Name: {{$produc_id=$product->product->name}}</p>
    <p> user id: {{$user_id=$product->user->id}}</p>
-   <p> product id: {{$produc_id=$product->product->id}}</p>
+   <p> product id: {{$product_id=$product->product->id}}</p>
+   <p> price: {{$product->product->price}}</p>
    <p>image:</p>
    <img src="{{ asset('storage/' . $product->product->image) }}" alt="{{ $product->product->name }} " class="max-w-full h-auto  md:w-[550px]" >
 
