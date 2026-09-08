984f5a0d2f7a1ff95d0c2ea698a4fb9eb494133b
jenou11 <jenousdongol11@gmail.com>
2026-09-08 20:51:19 +0545

    added cart to the hamburger link for mobile responsive view to be seen

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -20,7 +20,7 @@
                 <li><a href="">Services</a></li>
                 <li><a href="">By Meaning</a></li>
                 <li><a href="">By Crystal</a></li>
-                <li class=" bg"><a href="{{ route('cart.index') }}"> <span
+                <li class=" "><a href="{{ route('cart.index') }}"> <span
                             class="text-sm  grid  ml-4  absolute  text-black  mb-4 bg-white rounded-sm "
                             id="cartBadge">{{ auth()->check() ? auth()->user()->cartItems()->sum('quantity') : 0 }}</span>
                         <i class="fa-solid fa-cart-shopping " alt="cart"> </i>
@@ -102,6 +102,11 @@
             <li><a href="">Services</a></li>
             <li><a href="">By Meaning</a></li>
             <li><a href="">By Crystal</a></li>
+            <li class=" "><a href="{{ route('cart.index') }}"> <span
+                            class="text-sm  grid  ml-4  absolute  text-black  mb-4 bg-white rounded-sm "
+                            id="cartBadge">{{ auth()->check() ? auth()->user()->cartItems()->sum('quantity') : 0 }}</span>
+                        <i class="fa-solid fa-cart-shopping " alt="cart"> </i>
+                    </a></li>
             {{-- @auth
                 <li><a href="{{ route('adminproducts.create') }}">Create</a></li>
             @endauth --}}
